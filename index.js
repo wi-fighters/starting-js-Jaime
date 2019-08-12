f1253ce54c468621acf46706e262d26d4a13bdc2
Jaime Segura <jseguraweb@gmail.com>
2019-08-12 16:19:17 +0200

    make exercise 8: isFun?

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-let a = 3;
-let b = 8;
-let c = 23;
+let programmingLanguage = "Javascript";
+let isFun = true;
 
-console.log(a === b);
-console.log(b === c);
-console.log(a === c);
-
+console.log(programmingLanguage + " " + "is fun:" + " " + isFun);
 
