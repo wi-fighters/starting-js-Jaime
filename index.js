45cd210ffe78b314faa96d37f05b46bfd2e54fd4
Jaime Segura <jseguraweb@gmail.com>
2019-08-12 15:58:37 +0200

    make concatenation with new profile's information

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ let city = "Berlin";
 
 console.log("Exercise 2:" + " " + name, surname, age, job, city);
 
-
+console.log("Exercise 3:" + " " + name + " " + surname + " " + "is a" + " " + age + " " + "year old" + " " + job + " " + "who lives in" + " " + city);
 
 
 
