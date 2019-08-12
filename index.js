d51e932800cb491f9e2ccc8df2786970e66d1e22
Jaime Segura <jseguraweb@gmail.com>
2019-08-12 15:53:51 +0200

    add concatenation with Dave Grohl's info

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-var firstName = "Jaime"
-var lastName = "Segura"
+let firstName = "Jaime"
+let lastName = "Segura"
 
-console.log(firstName, lastName);
+console.log("Exercise 1:" + " " + firstName, lastName);
+
+let name = "Dave";
+let surname = "Grohl";
+let age = 50;
+let job = "rock star";
+let city = "Ohio";
+
+console.log("Exercise 2:" + " " + name, surname, age, job, city);
+
+console.log("Exercise 3:" + " " + name + " " + surname + " " + "is a" + " " + age + " " + "years old" + " " + job + " " + "who was born in" + " " + city + ".");
 
 
 
