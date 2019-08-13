5eb841474217d2938d17b806c22f29827337d5a6
Jaime Segura <jseguraweb@gmail.com>
2019-08-13 15:41:56 +0200

    add exercise 5: consumer formula

--- arithmetic/index.js
+++ arithmetic/index.js
@@ -24,7 +24,7 @@ const testThree = 85;
 const testFour = 90;
 const testFive = 100;
 
-console.log(`Expected output: Score in the sixth test: ${(average * numberOfTests) - (testOne + testTwo + testThree + testFour + testFive)}`);
+console.log(`Expected output: Score in the sixth test: ${(average * numberOfTests) - (testOne + testTwo + testThree + testFour + testFive)}`); // 90
 
 
 // EXERCISE 4: The minimum Score
@@ -34,4 +34,18 @@ const finalAssessments = 9;
 const actualAverage = 78;
 const actualAssessments = 8;
 
-console.log(`James needs a minimum of ${(finalAverage * finalAssessments) - (actualAverage * actualAssessments)}% to get an 80% average.`);
+console.log(`James needs a minimum of ${(finalAverage * finalAssessments) - (actualAverage * actualAssessments)}% to get an 80% average.`); // 96%
+
+
+// EXERCISE 5: Consumer Formula
+
+const billOne = 22.35;
+const tipOne = billOne * 0.10;
+const billTwo = 26.67;
+const tipTwo = billTwo * 0.15;
+const billThree = 35.92;
+const tipThree = billThree * 0.20;
+
+console.log(`The total amount of the first bill including 10% tip is: ${billOne + tipOne}`); // 24.58
+console.log(`The total amount of the second bill including 15% tip is: ${billTwo + tipTwo}`); // 30.67
+console.log(`The total amount of the third bill including 20% tip is: ${billThree + tipThree}`); // 43.10
