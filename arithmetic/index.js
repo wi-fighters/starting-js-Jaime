14fbc6317be259d550de0a7c73df07e922cc6688
Jaime Segura <jseguraweb@gmail.com>
2019-08-13 14:14:37 +0200

    add exercise 2: working with averages

--- arithmetic/index.js
+++ arithmetic/index.js
@@ -7,3 +7,10 @@ const percentage = (firstNumber / secondNumber) / 100;
 console.log(`${percentage}%`); // 30 is 0,0056% of 17
 
 
+// EXERCISE 2: Working with averages
+
+const workingHours = 8 + 6 + 5 + 9 + 8 + 2 + 1 + 8.5 + 7 + 4;
+const workingDays = 10;
+console.log(`Noemy's average hour is: ${workingHours / workingDays}`); // 5.85 hours
+
+
