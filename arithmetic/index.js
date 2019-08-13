ad612f861e47c3131e8deaaa39faa6cad306cf9d
Jaime Segura <jseguraweb@gmail.com>
2019-08-13 13:55:41 +0200

    add exercise 1

---
+++ arithmetic/index.js
@@ -0,0 +1,9 @@
+// EXERCISE 1: Percent
+
+const firstNumber = 17;
+const secondNumber = 30;
+const percentage = (firstNumber / secondNumber) / 100;
+
+console.log(`${percentage}%`); // 30 is 0,0056% of 17
+
+
