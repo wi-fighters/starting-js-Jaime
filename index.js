5dd24aab1e89375ed298fe301b6e67487c6a5f8e
Jaime Segura <jseguraweb@gmail.com>
2019-08-13 09:50:14 +0200

    try to pull and push the file

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-let programmingLanguage = "Javascript";
-let isFun = true;
+let a = 3;
+let b = 8;
+let c = 23;
 
-console.log(programmingLanguage + " " + "is fun:" + " " + isFun);
+console.log(a === b);
+console.log(b === c);
+console.log(a === c);
+
+console.log(typeof a, b, c);
+
 
