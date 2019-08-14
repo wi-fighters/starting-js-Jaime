2ac3f5c5f00b50d64bcb6212d2af0f961e003488
Jaime Segura <jseguraweb@gmail.com>
2019-08-14 14:58:58 +0200

    add exercise 2: JS

--- string-methods/index.js
+++ string-methods/index.js
@@ -1,7 +1,16 @@
 // 1
 
 const name = "Natalia";
-const secondElem = (name[1]);
-const sixthElem = (name[5]);
-console.log(name.length);
-console.log(secondElem, sixthElem);
+console.log(`Name length is ${name.length}`);
+const secondElem = name[1];
+const sixthElem = name[5];
+console.log(`2nd element: ${secondElem}, 6th element: ${sixthElem}`);
+
+
+// 2
+
+const js = "JavaScript";
+console.log(`The length of the string is: ${js.length} characters`);
+const letterJ = js[0];
+const letterS = js[4];
+console.log(`${letterJ} & ${letterS}`);
