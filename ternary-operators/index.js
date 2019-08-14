e9d150e2b79b9c40e9c9eb492d34b0c62aad8d22
Jaime Segura <jseguraweb@gmail.com>
2019-08-14 11:07:19 +0200

    modify exercise 2

--- ternary-operators/index.js
+++ ternary-operators/index.js
@@ -1,6 +1,6 @@
 // 1. canDrive?
 
-const canDrive = 31;
+const canDrive = 15;
 const legalAge = 18;
 
 console.log((canDrive >= legalAge)? "Yes, you can drive! Hope you have a license": "Nope, you're too young!");
@@ -8,21 +8,22 @@ console.log((canDrive >= legalAge)? "Yes, you can drive! Hope you have a license
 
 // 2. Slow Down!
 
-const speedCheck = 120;
+const speedCheck = 135;
+const maxSpeed = 130;
 
-console.log((speedCheck > 130)? "You're going too fast - slow down": "You're driving below the speed limit");
+console.log((speedCheck > maxSpeed)? "You're going too fast - slow down": "You're driving below the speed limit");
 
 
 // 3. How old are you?
 
-const personAge = 25;
+const personAge = 20;
 
 console.log((personAge < 21)? "Here's some juice": "Here's some wine");
 
 
 // 4. Student Discount
 
-const isStudent = 22;
+const isStudent = 28;
 
 console.log((isStudent >= 18 && isStudent <= 25)? "Ticket costs €5,00": "Ticket costs €12,00");
 
