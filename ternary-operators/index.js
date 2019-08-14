d54061c52b8599d83c59cc1a28a1035d49451bb1
Jaime Segura <jseguraweb@gmail.com>
2019-08-14 09:57:18 +0200

    add exercise 2

---
+++ ternary-operators/index.js
@@ -0,0 +1,13 @@
+// 1. canDrive?
+
+const canDrive = 31;
+const legalAge = 18;
+
+console.log((canDrive >= legalAge)? "Yes, you can drive! Hope you have a license": "Nope, you're too young!");
+
+
+// 2. Slow Down!
+
+const speedCheck = 120;
+
+console.log((speedCheck > 130)? "You're going too fast - slow down": "You're driving below the speed limit");
