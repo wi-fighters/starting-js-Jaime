86514f27e277e71a305dc027d6d14863cd116d0b
Jaime Segura <jseguraweb@gmail.com>
2019-08-14 15:14:46 +0200

    modify exercise 3: I can...

--- string-methods/index.js
+++ string-methods/index.js
@@ -18,6 +18,17 @@ console.log(`${letterJ} & ${letterS}`);
 
 // 3
 
+// const sentence = "I can walk in the park all day!";
+// console.log(`${sentence.substring(18, 22)}`);
+
 const sentence = "I can walk in the park all day!";
-console.log(`${sentence.substring(18, 22)}`);
+const goal = sentence.includes("park");
+const park = (goal)? "park" : "keep trying";
+console.log(`${park}`);
+
+
+//4
+
+const jsWord = "JavaScript";
+console.log(`${jsWord.substring(3, 6)}`);
 
