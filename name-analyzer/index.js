fd23a7478abe01d722ff3103da3710248846bb74
jseguraweb <jseguraweb@gmail.com>
2019-08-16 19:06:40 +0200

    add Winston exercise

---
+++ name-analyzer/index.js
@@ -0,0 +1,8 @@
+// Winston Exercise
+
+const name = "Winston";
+
+console.log(`Hello ${name}.
+Your name is ${name.length} letters long.
+The first letter of your name is ${name[0]}, the last is ${name.substring(name.length - 1)}.
+If I wanted to be funny, I would call you ${name.substring(0,3)}chmoo.`);
