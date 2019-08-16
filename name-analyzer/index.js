0c3764a8dd7b77e75d60707205634cd858e42d23
jseguraweb <jseguraweb@gmail.com>
2019-08-16 19:16:32 +0200

    modify Winston exercise

--- name-analyzer/index.js
+++ name-analyzer/index.js
@@ -1,8 +1,9 @@
 // Winston Exercise
 
-const name = "Winston";
+const name = " Winston  ";
+const correctName = name.replace(/\s/g, "");
 
-console.log(`Hello ${name}.
-Your name is ${name.length} letters long.
-The first letter of your name is ${name[0]}, the last is ${name.substring(name.length - 1)}.
-If I wanted to be funny, I would call you ${name.substring(0,3)}chmoo.`);
+console.log(`Hello ${name.trim()}.
+Your name is ${correctName.length} letters long.
+The first letter of your name is ${correctName[0]}, the last is ${correctName.substring(correctName.length - 1)}.
+If I wanted to be funny, I would call you ${correctName.substring(0,3)}chmoo.`);
