ad3b27545b4cfbe7f734e6ce3f7a7b5f0eb6da50
Jaime Segura <jseguraweb@gmail.com>
2019-08-19 16:06:32 +0200

    add exercise number 3: round up and round down

--- math-object/index.js
+++ math-object/index.js
@@ -13,3 +13,20 @@ console.log(`The youngest friend in the group is ${Math.min(...friends)} years o
 // 2. Highest Number
 
 console.log(`The oldest friend in the group is ${Math.max(...friends)} years old.`);
+
+
+// 3. Rounding
+// 3.1. Round up
+
+const numberOne = 3321.32321;
+const numberTwo = 326.76;
+const numberThree = 76788.7;
+const numberFour = -9.78;
+const numberFive = 43.342;
+
+console.log(`Rounding up the given numbers: ${Math.ceil(numberOne)}, ${Math.ceil(numberTwo)}, ${Math.ceil(numberThree)}, ${Math.ceil(numberFour)}, ${Math.ceil(numberFive)}`);
+
+
+// 3.2 Round down
+
+console.log(`Rounding down the given numbers: ${Math.floor(numberOne)}, ${Math.floor(numberTwo)}, ${Math.floor(numberThree)}, ${Math.floor(numberFour)}, ${Math.floor(numberFive)}`);
