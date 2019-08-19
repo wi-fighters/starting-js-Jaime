6d3cb6fe9c9495f755310bdf955551eb106eba97
Jaime Segura <jseguraweb@gmail.com>
2019-08-19 15:21:45 +0200

    add exercise 2: highest number

--- math-object/index.js
+++ math-object/index.js
@@ -1,4 +1,9 @@
-// 1 Lowest Number
+// 1. Lowest Number
 
 const numbers = [2, 56, 3.5, 9.71, 33.3, 87.22];
 console.log(Math.min(...numbers));
+
+
+// 2. Highest Number
+
+console.log(Math.max(...numbers));
