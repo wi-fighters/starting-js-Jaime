7060051db332c4281208e4fdb6ca63c983d17b02
Jaime Segura <jseguraweb@gmail.com>
2019-08-19 15:26:50 +0200

    modify exercises 1 and 2

--- math-object/index.js
+++ math-object/index.js
@@ -1,9 +1,15 @@
 // 1. Lowest Number
 
-const numbers = [2, 56, 3.5, 9.71, 33.3, 87.22];
-console.log(Math.min(...numbers));
+const Peter = 28;
+const John = 32;
+const Sarah = 27;
+const Hannah = 33;
+const Michael = 25;
+const friends = [Peter, John, Sarah, Hannah, Michael];
+
+console.log(`The youngest friend in the group is ${Math.min(...friends)} years old.`);
 
 
 // 2. Highest Number
 
-console.log(Math.max(...numbers));
+console.log(`The oldest friend in the group is ${Math.max(...friends)} years old.`);
