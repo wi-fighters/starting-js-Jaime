7e0bcf8c6590ecafcadd6d17588858579a56fdd6
Jaime Segura <jseguraweb@gmail.com>
2019-08-20 11:02:11 +0200

    modify text from conceptual understanding

--- math-object/index.js
+++ math-object/index.js
@@ -10,6 +10,7 @@ const friends = [Peter, John, Sarah, Hannah, Michael];
 console.log(`The youngest friend in the group is ${Math.min(...friends)} years old.`);
 
 
+
 // 2. Highest Number
 
 console.log(`The oldest friend in the group is ${Math.max(...friends)} years old.`);
@@ -24,7 +25,7 @@ const numberThree = 76788.7;
 const numberFour = -9.78;
 const numberFive = 43.342;
 
-console.log(`Rounding up the given numbers: ${Math.ceil(numberOne)}, ${Math.ceil(numberTwo)}, ${Math.ceil(numberThree)}, ${Math.ceil(numberFour)}, ${Math.ceil(numberFive)}`);
+console.log(`Rounding up the given numbers: ${Math.ceil(numberOne)}, ${Math.ceil(numberTwo)}, ${Math.ceil(numberThree)}, ${Math.ceil(numberFour)}, ${Math.round(numberFive)}`);
 
 
 // 3.2 Round down
@@ -49,8 +50,24 @@ console.log(Math.floor(Math.random()*(maxNum - minNum + 1) + minNum));
 // What is a string interpolation?
 // It is a really useful programming language feature that allows you to inject variables directly into a string. Example: (`This is my ${variable}`)
 //
-// There is something I tried and didn't manage. In the 1st exercise I wanted to print the name of the variable with the lowest number. I tried like this:
+//
+// SOMETHING NEW I LEARNT: extract the lowest or the highest number FROM A STRING using "..." + variable
+//
+// const numbers = [19, 3, 22, 25, 2];
+// console.log(Math.min(...numbers)); --> here you will get "2"
+//
+// It is mandatory to type the "..." in this case. If you don't do it, you just get "NaN":
+//
+// const numbers = [19, 3, 22, 25, 2];
+// console.log(Math.min(numbers)); --> here you will get "NaN"
+//
+//
+// There is something I tried and DIDN'T MANAGE. In the 1st exercise I wanted to print the name of the variable with the lowest number. I tried like this:
+//
 // console.log(`The youngest friend in the group is ${Math.min(...friends).name} years old.`);
+//
 // and like this:
+//
 // console.log(`The youngest friend in the group is ${(Math.min(...friends)).name} years old.`);
+//
 // but the console returned "undefined" in both of the cases.
