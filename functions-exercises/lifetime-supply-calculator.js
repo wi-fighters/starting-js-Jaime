6afd157c9284c18d0faed473530d294b9162eae1
Jaime Segura <jseguraweb@gmail.com>
2019-08-27 21:48:49 +0200

    add bonus: accept floating values for amount per day and round the result

--- functions-exercises/lifetime-supply-calculator.js
+++ functions-exercises/lifetime-supply-calculator.js
@@ -1,9 +1,9 @@
 function calculateSupply(age, amountPerDay) {
     const deathAge = 100;
     const lifeAmount = amountPerDay * 365 * (deathAge - age);
-    return `You will need ${lifeAmount} Kit-Kats to last you until the ripe old age of ${deathAge}.`
+    return `You will need ${Math.ceil(lifeAmount)} Kit-Kats to last you until the ripe old age of ${deathAge}.`
 }
 
-console.log(calculateSupply(31, 3.1));
-console.log(calculateSupply(52, 2.9));
-console.log(calculateSupply(98, 1.6));
+console.log(calculateSupply(31, 3.454));
+console.log(calculateSupply(52, 2.456));
+console.log(calculateSupply(98, 1.945));
