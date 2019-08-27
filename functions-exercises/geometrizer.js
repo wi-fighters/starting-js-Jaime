69b690205ca92d9ec67b742c93d37946aba38df3
Jaime Segura <jseguraweb@gmail.com>
2019-08-27 21:51:55 +0200

    add 2 functions to calculate circumfrence and area of a circle

---
+++ functions-exercises/geometrizer.js
@@ -0,0 +1,12 @@
+function calcCircumfrence(radius) {
+
+    return `The circumfrence is NN cm.`
+}
+
+function calcArea(radius) {
+
+    return `The area is NN cm².`
+}
+
+console.log(calcCircumfrence(5));
+console.log(calcArea(5));
