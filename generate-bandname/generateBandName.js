96add25967f35af314c3346f0cc5c50fc29eba6b
Jaime Segura <jseguraweb@gmail.com>
2019-08-28 19:36:29 +0200

    add first try

--- generate-bandname/generateBandName.js
+++ generate-bandname/generateBandName.js
@@ -1,9 +1,12 @@
 // Every time the function is called, it should store the result in a global array (and still return it like before)
 
+const myArrayStore = [];
+
 function newBand(fruit, car) {
     const fruitUpperCase = fruit[0].toUpperCase() + fruit.substring(1);
     const carUppercase = car[0].toUpperCase() + car.substring(1);
-    return `The name of our new rock band is "${fruitUpperCase} ${carUppercase}"!`;
+    return `${fruitUpperCase} ${carUppercase}`;
 }
 
-console.log(newBand("banana", "peugeot"));
+console.log(myArrayStore.push(newBand("orange", "peugeot")));
+console.log(myArrayStore);
