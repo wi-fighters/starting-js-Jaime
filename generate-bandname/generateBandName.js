f2a8fb7164732096c1c300f303d28be116c5ef12
Jaime Segura <jseguraweb@gmail.com>
2019-09-01 20:24:31 +0200

    modify function

--- generate-bandname/generateBandName.js
+++ generate-bandname/generateBandName.js
@@ -6,9 +6,9 @@ console.log(createBand("shirt", "peaches"));
 
 
 function newBand(fruit, car) {
-    const fruitUpperCase = fruit[0].toUpperCase() + fruit.substring(1);
-    const carUppercase = car[0].toUpperCase() + car.substring(1);
+    const fruitUpperCase = fruit[0].toUpperCase() + fruit.substring(1).toLowerCase();
+    const carUppercase = car[0].toUpperCase() + car.substring(1).toLowerCase();
     return `The name of our new rock band is "${fruitUpperCase} ${carUppercase}"!`;
 }
 
-console.log(newBand("banana", "peugeot"));
+console.log(newBand("banAna", "peugEot"));
