8008df38e05055554471e47bd08946ce9d903da5
Jaime Segura <jseguraweb@gmail.com>
2019-09-02 14:00:06 +0200

    feature branch merged into master

--- generate-bandname/generateBandName.js
+++ generate-bandname/generateBandName.js
@@ -1,14 +1,24 @@
-function createBand(clothing, lastFoodEaten) {
-    return `The name of our band is "The ${clothing} ${lastFoodEaten}"!`;
-}
-
-console.log(createBand("shirt", "peaches"));
-
+// Return a string that represents the resulting band name, with every first letter as a capital.
 
 function newBand(fruit, car) {
+
     const fruitUpperCase = fruit[0].toUpperCase() + fruit.substring(1).toLowerCase();
     const carUppercase = car[0].toUpperCase() + car.substring(1).toLowerCase();
-    return `The name of our new rock band is "${fruitUpperCase} ${carUppercase}"!`;
+    return `${fruitUpperCase} ${carUppercase}`;
 }
 
-console.log(newBand("banAna", "peugEot"));
+console.log(`The name of our rockband is ${newBand("banAna", "peugEot")}`);
+
+
+// Every time the function is called, it should store the result in a global array (and still return it like before)
+
+const myArrayStore = [];
+
+console.log(myArrayStore.unshift(newBand("banaNA", "pEUgeot")));
+console.log(myArrayStore);
+
+console.log(myArrayStore.unshift(newBand("strawBErRy", "OPel")));
+console.log(myArrayStore);
+
+console.log(myArrayStore.unshift(newBand("orAnge", "mercEDes")));
+console.log(myArrayStore);
