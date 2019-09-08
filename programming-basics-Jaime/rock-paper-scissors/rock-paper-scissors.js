c78b77d39d6d3f0a0611c587a26b250f0ef21567
Jaime Segura <jseguraweb@gmail.com>
2019-09-08 21:36:09 +0200

    add ex3: Two-player

--- programming-basics-Jaime/rock-paper-scissors/rock-paper-scissors.js
+++ programming-basics-Jaime/rock-paper-scissors/rock-paper-scissors.js
@@ -26,27 +26,70 @@
 
 // Honest Rock Paper Scissors
 
+// const options = ["rock", "paper", "scissors"];
+
+// const attack = (randomHand) => {
+//     randomHand = rightWritten(randomHand);
+//     switch (randomHand) {
+//         case "rock":
+//         case "paper":
+//         case "scissors":
+//             return takeRandom(options);
+//     }
+//     return `I'm an honest function bro, be honest too and just write "rock", "paper", or "scissors" as an argument`;
+// };
+
+// console.log(attack("scissors"));
+
+// function rightWritten(rawString) {
+
+//     return rawString.toString().toLowerCase();
+// }
+
+// function takeRandom(number) {
+
+//     return number[Math.floor(Math.random() * 3)];
+// }
+
+
+// Two-player
+
 const options = ["rock", "paper", "scissors"];
 
-const attack = (randomHand) => {
-    randomHand = rightWritten(randomHand);
-    switch (randomHand) {
-        case "rock":
-        case "paper":
-        case "scissors":
-            return takeRandom(options);
+const attack = (playerA, playerB) => {
+    playerA = rightWritten(playerA);
+    playerB = rightWritten(playerB);
+
+    console.log(`Player A plays ${playerA} and Player B plays ${playerB}`);
+
+    if (!options.includes(playerA) || !options.includes(playerB)) {
+        return `I only take "rock", "paper" or "scissors" as an argument`;
+    } else if (playerA === playerB) {
+        return `It's a draw!`;
+    } else if (playerA === "rock" && playerB === "paper") {
+        return `Player B wins!`;
+    } else if (playerA === "rock" && playerB === "scissors") {
+        return `Player A wins!`;
+    } else if (playerA === "paper" && playerB === "rock") {
+        return `Player A wins!`;
+    } else if (playerA === "paper" && playerB === "scissors") {
+        return `Player A wins!`;
+    } else if (playerA === "scissors" && playerB === "rock") {
+        return `Player B wins!`;
+    } else if (playerA === "scissors" && playerB === "paper") {
+        return `Player A wins!`;
     }
-    return `I'm an honest function bro, be honest too and just write "rock", "paper", or "scissors" as an argument`;
 };
 
-console.log(attack("scissors"));
+console.log(attack("scIssors", "papEr"));
+console.log(attack("rOck", "paPer"));
+console.log(attack("SCissors", "sciSSors"));
+console.log(attack("3", "5"));
+console.log(attack("1", "pAper"));
+console.log(attack("sCissors", "2"));
+
 
 function rightWritten(rawString) {
 
     return rawString.toString().toLowerCase();
-}
-
-function takeRandom(number) {
-
-    return number[Math.floor(Math.random() * 3)];
 }
