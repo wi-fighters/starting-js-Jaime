fa77bfe34b66ee8becbc744e934bb980de254769
Jaime Segura <jseguraweb@gmail.com>
2019-09-08 21:05:04 +0200

    add ex2: Honest Rock Paper Scissors

--- programming-basics-Jaime/rock-paper-scissors/rock-paper-scissors.js
+++ programming-basics-Jaime/rock-paper-scissors/rock-paper-scissors.js
@@ -1,22 +1,52 @@
+// Unbeatable Rock Paper Scissors
+
+// const attack = (hand) => {
+
+//     hand = rightWritten(hand);
+
+//     switch (hand) {
+//         case "scissors":
+//             return "rock";
+//         case "rock":
+//             return "paper";
+//         case "paper":
+//             return "scissors";
+//         default:
+//             return `I only take "rock", "paper" or "scissors" as an argument`;
+//     }
+// };
+
+// console.log(attack(3));
+
+// function rightWritten(rawString) {
+
+//     return rawString.toString().toLowerCase();
+// }
+
+
+// Honest Rock Paper Scissors
+
+const options = ["rock", "paper", "scissors"];
+
+const attack = (randomHand) => {
+    randomHand = rightWritten(randomHand);
+    switch (randomHand) {
+        case "rock":
+        case "paper":
+        case "scissors":
+            return takeRandom(options);
+    }
+    return `I'm an honest function bro, be honest too and just write "rock", "paper", or "scissors" as an argument`;
+};
+
+console.log(attack("scissors"));
+
 function rightWritten(rawString) {
 
     return rawString.toString().toLowerCase();
 }
 
-const attack = (hand) => {
+function takeRandom(number) {
 
-    hand = rightWritten(hand);
-
-    switch (hand) {
-        case "scissors":
-            return "rock";
-        case "rock":
-            return "paper";
-        case "paper":
-            return "scissors";
-        default:
-            return `I only take "rock", "paper" or "scissors" as an argument`;
-    }
+    return number[Math.floor(Math.random() * 3)];
 }
-
-console.log(attack(3));
