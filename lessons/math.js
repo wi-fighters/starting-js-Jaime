67c66414c9efc80600d515efa5fef6a8345b27e0
Jaime Segura <jseguraweb@gmail.com>
2019-09-08 22:04:33 +0200

    reorganize folders

---
+++ lessons/math.js
@@ -0,0 +1,21 @@
+//Math.round()
+
+console.log(Math.round(1.5)); // 2
+console.log(Math.round(1.51)); // 2
+console.log(Math.round(1.49)); // 1
+
+//Math.ceil()
+
+console.log(Math.ceil(1.01)); // 2
+
+//Math.floor()
+
+console.log(Math.floor(2.99)); // 2
+
+//Math.min()
+
+console.log(Math.min(12, 350)); // 12
+
+//Math.max()
+
+console.log(Math.max(12, 350)); // 350
