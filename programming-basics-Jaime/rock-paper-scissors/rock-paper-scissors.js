0f65402e99e7e6f3a2a0d9705ea795883d893958
Jaime Segura <jseguraweb@gmail.com>
2019-09-09 10:12:49 +0200

    add 'array.length' to get a random index number

--- programming-basics-Jaime/rock-paper-scissors/rock-paper-scissors.js
+++ programming-basics-Jaime/rock-paper-scissors/rock-paper-scissors.js
@@ -26,30 +26,30 @@
 
 // Honest Rock Paper Scissors
 
-// const options = ["rock", "paper", "scissors"];
+const options = ["rock", "paper", "scissors"];
 
-// const attack = (randomHand) => {
-//     randomHand = rightWritten(randomHand);
-//     switch (randomHand) {
-//         case "rock":
-//         case "paper":
-//         case "scissors":
-//             return takeRandom(options);
-//     }
-//     return `I'm an honest function bro, be honest too and just write "rock", "paper", or "scissors" as an argument`;
-// };
+const attack = (randomHand) => {
+    randomHand = rightWritten(randomHand);
+    switch (randomHand) {
+        case "rock":
+        case "paper":
+        case "scissors":
+            return takeRandom(options);
+    }
+    return `I'm an honest function bro, be honest too and just write "rock", "paper", or "scissors" as an argument`;
+};
 
-// console.log(attack("scissors"));
+console.log(attack("Paper"));
 
-// function rightWritten(rawString) {
+function rightWritten(rawString) {
 
-//     return rawString.toString().toLowerCase();
-// }
+    return rawString.toString().toLowerCase();
+}
 
-// function takeRandom(number) {
+function takeRandom(number) {
 
-//     return number[Math.floor(Math.random() * 3)];
-// }
+    return number[Math.floor(Math.random() * number.length)];
+}
 
 
 // Two-player
@@ -97,80 +97,80 @@
 
 //Rock Paper Scissors Lizard Spock(bonus)
 
-const options = ["rock", "paper", "scissors", "lizard", "spock"];
+// const options = ["rock", "paper", "scissors", "lizard", "spock"];
 
-const attack = (playerA, playerB) => {
-    playerA = rightWritten(playerA);
-    playerB = rightWritten(playerB);
+// const attack = (playerA, playerB) => {
+//     playerA = rightWritten(playerA);
+//     playerB = rightWritten(playerB);
 
-    console.log(`Player A plays ${playerA} and Player B plays ${playerB}`);
+//     console.log(`Player A plays ${playerA} and Player B plays ${playerB}`);
 
-    if (!options.includes(playerA) || !options.includes(playerB)) {
-        return `I only take "rock", "paper" or "scissors" as an argument`;
-    } else if (playerA === playerB) {
-        return `It's a draw!`;
-    } else {
-        switch (playerA) {
-            case "rock":
-                switch (playerB) {
-                    case "scissors":
-                    case "lizard":
-                        return `Player A wins!`;
-                    case "spock":
-                    case "paper":
-                        return `Player B wins!`
-                }
-            case "paper":
-                switch (playerB) {
-                    case "scissors":
-                    case "lizard":
-                        return `Player A wins!`;
-                    case "rock":
-                    case "spock":
-                        return `Player B wins!`
-                }
-            case "scissors":
-                switch (playerB) {
-                    case "paper":
-                    case "lizard":
-                        return `Player A wins!`;
-                    case "rock":
-                    case "spock":
-                        return `Player B wins!`
-                }
-            case "lizard":
-                switch (playerB) {
-                    case "paper":
-                    case "spock":
-                        return `Player A wins!`;
-                    case "rock":
-                    case "scissors":
-                        return `Player B wins!`
-                }
-            case "spock":
-                switch (playerB) {
-                    case "rock":
-                    case "scissors":
-                        return `Player A wins!`;
-                    case "lizard":
-                    case "paper":
-                        return `Player B wins!`
-                }
-        }
-    }
-};
+//     if (!options.includes(playerA) || !options.includes(playerB)) {
+//         return `I only take "rock", "paper" or "scissors" as an argument`;
+//     } else if (playerA === playerB) {
+//         return `It's a draw!`;
+//     } else {
+//         switch (playerA) {
+//             case "rock":
+//                 switch (playerB) {
+//                     case "scissors":
+//                     case "lizard":
+//                         return `Player A wins!`;
+//                     case "spock":
+//                     case "paper":
+//                         return `Player B wins!`
+//                 }
+//             case "paper":
+//                 switch (playerB) {
+//                     case "scissors":
+//                     case "lizard":
+//                         return `Player A wins!`;
+//                     case "rock":
+//                     case "spock":
+//                         return `Player B wins!`
+//                 }
+//             case "scissors":
+//                 switch (playerB) {
+//                     case "paper":
+//                     case "lizard":
+//                         return `Player A wins!`;
+//                     case "rock":
+//                     case "spock":
+//                         return `Player B wins!`
+//                 }
+//             case "lizard":
+//                 switch (playerB) {
+//                     case "paper":
+//                     case "spock":
+//                         return `Player A wins!`;
+//                     case "rock":
+//                     case "scissors":
+//                         return `Player B wins!`
+//                 }
+//             case "spock":
+//                 switch (playerB) {
+//                     case "rock":
+//                     case "scissors":
+//                         return `Player A wins!`;
+//                     case "lizard":
+//                     case "paper":
+//                         return `Player B wins!`
+//                 }
+//         }
+//     }
+// };
 
-console.log(attack("scIssors", "papEr"));
-console.log(attack("rOck", "paPer"));
-console.log(attack("SCissors", "sciSSors"));
-console.log(attack("sPocK", "Lizard"));
-console.log(attack("liZard", "paPer"));
-console.log(attack("Rock", "sPOck"));
-console.log(attack("3", "5"));
-console.log(attack(1, "pAper"));
-console.log(attack("sCissors", 2));
+// console.log(attack("scIssors", "papEr"));
+// console.log(attack("rOck", "paPer"));
+// console.log(attack("SCissors", "sciSSors"));
+// console.log(attack("sPocK", "Lizard"));
+// console.log(attack("liZard", "paPer"));
+// console.log(attack("Rock", "sPOck"));
+// console.log(attack("3", "5"));
+// console.log(attack(1, "pAper"));
+// console.log(attack("sCissors", 2));
 
-function rightWritten(rawString) {
+// function rightWritten(rawString) {
 
-    return rawString.toString().toLowerCase();
-}
+//     return rawString.toString().toLowerCase();
+// }
