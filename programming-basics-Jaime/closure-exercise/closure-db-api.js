90815ab79c4b5b688f240627f4ae20b61f8bfd7b
Jaime Segura <jseguraweb@gmail.com>
2019-09-12 21:20:09 +0200

    add complete part 3: adding an update function and testing it

--- programming-basics-Jaime/closure-exercise/closure-db-api.js
+++ programming-basics-Jaime/closure-exercise/closure-db-api.js
@@ -33,16 +33,30 @@ const init = () => {
 
 const createInDb = init();
 
+console.log(`\nChecking out what's in my database:`)
+console.log(createInDb[1]());
+
 createInDb[0]("Bread");
 createInDb[0]("Butter");
 createInDb[0]("Ham");
 createInDb[0]("Cheese");
 
+console.log(`\nChecking out my database after pushing 4 items inside it:`)
 console.log(createInDb[1]());
+
+console.log(`\nI'm gonna push a new item ('Milk') inside the database:`)
 console.log(createInDb[0]("Milk"));
 console.log(createInDb[1]());
 
-console.log(createInDb[2](2, "Chocolate"));
+console.log(`\nNow I'm gonna push another new item ('Banana') inside the database:`)
+console.log(createInDb[0]("Banana"));
+console.log(createInDb[1]());
+
+console.log(`\nNow I'm gonna update the first item: I'll change 'Bread' for 'Chocolate'`)
+console.log(createInDb[2](0, "Chocolate"));
+
+console.log(`\nNow I'm gonna update the third item: I'll change 'Ham' for 'Vanilla'`)
 console.log(createInDb[2](2, "Vanilla"));
-console.log(createInDb[2](5, "Eis"));
+
+console.log(`\nChecking out my database again:`)
 console.log(createInDb[1]());
