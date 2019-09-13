83230350db1b268b103cf8c904bff1a0a259edc6
Jaime Segura <jseguraweb@gmail.com>
2019-09-13 10:22:52 +0200

    assign functions of the array api to 4 different const

--- programming-basics-Jaime/closure-exercise/closure-db-api.js
+++ programming-basics-Jaime/closure-exercise/closure-db-api.js
@@ -44,35 +44,40 @@ const init = () => {
 
 const createInDb = init();
 
+const createItem = createInDb[0];
+const readArray = createInDb[1];
+const updateItem = createInDb[2];
+const removeItem = createInDb[3];
+
 console.log(`\nChecking out what's in my database:`)
 console.log(createInDb[1]());
 
-createInDb[0]("Bread");
-createInDb[0]("Butter");
-createInDb[0]("Ham");
-createInDb[0]("Cheese");
+createItem("Bread");
+createItem("Butter");
+createItem("Ham");
+createItem("Cheese");
 
 console.log(`\nChecking out my database after pushing 4 items inside it:`)
-console.log(createInDb[1]()); // read
+console.log(readArray()); // read
 
 console.log(`\nI'm gonna push a new item ('Milk') inside the database:`)
-console.log(createInDb[0]("Milk")); // 
-console.log(createInDb[1]()); // read
+console.log(createItem("Milk")); // 
+console.log(readArray()); // read
 
 console.log(`\nNow I'm gonna push another new item ('Banana') inside the database:`)
-console.log(createInDb[0]("Banana"));
-console.log(createInDb[1]()); // read
+console.log(createItem("Banana"));
+console.log(readArray()); // read
 
 console.log(`\nNow I'm gonna update the first item: I'll change 'Bread' for 'Chocolate'`)
-console.log(createInDb[2](0, "Chocolate"));
-console.log(createInDb[1]()); // read
+console.log(updateItem(0, "Chocolate"));
+console.log(readArray()); // read
 
 console.log(`\nNow I'm gonna update the third item: I'll change 'Ham' for 'Vanilla'`)
-console.log(createInDb[2](2, "Vanilla"));
-console.log(createInDb[1]()); // read
+console.log(updateItem(2, "Vanilla"));
+console.log(readArray()); // read
 
 console.log(`\nI'm gonna delete the 'Cheese' (item 4 from my database)`)
-console.log(createInDb[3](3)); // delete
+console.log(removeItem(3)); // delete
 
 console.log(`\nThis is the current status of my database:`)
-console.log(createInDb[1]());
+console.log(readArray()); // read
