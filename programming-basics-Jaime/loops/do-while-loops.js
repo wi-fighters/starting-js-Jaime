89d9a251181aec605d22244fbac44d207a8c2fc5
Jaime Segura <jseguraweb@gmail.com>
2019-09-17 11:49:13 +0200

    add exercise 2 do-while-loops: add it up

--- programming-basics-Jaime/loops/do-while-loops.js
+++ programming-basics-Jaime/loops/do-while-loops.js
@@ -2,14 +2,30 @@
 
 // 1. sumOfNumbers. Create a program that adds up the numbers in an array (of at least 3 numbers).
 
-const myNumbers = [3, 23, 19, 210, 56];
+// const myNumbers = [3, 23, 19, 210, 56];
+// let i = 0;
+// let sumOfNumbers = 0;
+
+// do {
+//     sumOfNumbers += myNumbers[i];
+//     ++i;
+// }
+// while (i < myNumbers.length);
+
+// console.log(sumOfNumbers);
+
+
+// 2. Add it up! Create a program that adds numbers from 1-20 together using a while loop.
+
+let myNumber = 0;
 let i = 0;
-let sumOfNumbers = 0;
 
 do {
-    sumOfNumbers += myNumbers[i];
     ++i;
-}
-while (i < myNumbers.length);
+    myNumber += i;
 
-console.log(sumOfNumbers);
+}
+while (i < 20)
+
+console.log(myNumber);
+
