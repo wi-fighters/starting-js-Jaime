30695b2d46830b213826395e39e9cdac7f4e4bb8
Jaime Segura <jseguraweb@gmail.com>
2019-09-18 11:48:40 +0200

    add exercise 6 for loops: FizzBuzz

--- programming-basics-Jaime/loops/for-loops.js
+++ programming-basics-Jaime/loops/for-loops.js
@@ -95,25 +95,21 @@ averageMark();
 
 // 6. Fizz Buzz Write a program which iterates the integers from 1 to 100. But for multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz". For numbers which are multiples of both three and five print "FizzBuzz".
 
-// const fizzBuzz = () => {
+const fizzBuzz = () => {
 
-//     let fizz = "Fizz";
-//     let buzz = "Buzz";
-//     let fizzBuzz = "FizzBuzz";
+    for (i = 1; i <= 100; ++i) {
+        let number = i;
 
-//     for (i = 1; i <= 100; ++i) {
-//         switch (i) {
-//             case (i % 3 === 0):
-//                 console.log(fizz);
-//             case (i % 5 === 0):
-//                 console.log(buzz);
-//             case (i % 3 === 0 && i % 5 === 0):
-//                 console.log(fizzBuzz);
-//             default:
-//                 console.log(i);
-//         }
-//     }
+        if (i % 3 === 0 && i % 5 === 0) {
+            number = "FizzBuzz";
+        } else if (i % 3 === 0) {
+            number = "Fizz";
+        } else if (i % 5 === 0) {
+            number = "Buzz"
+        }
+        console.log(number);
+    }
 
-// };
-// console.log(`\nExercise 6: Fizz Buzz`)
-// fizzBuzz();
+};
+console.log(`\nExercise 6: Fizz Buzz`)
+fizzBuzz();
