df7f970facb08fc08e6a8dac80f68e31265fb7ec
Jaime Segura <jseguraweb@gmail.com>
2019-09-18 09:57:22 +0200

    add exercise 1 for loops: addition

--- programming-basics-Jaime/loops/do-while-loops.js
+++ programming-basics-Jaime/loops/do-while-loops.js
@@ -32,18 +32,17 @@
 
 // 3.Capitalize. Create a program that capitalizes the first letter of each element in an array of names, using a do while loop
 
-// const myCities = ["maDriD", "MáLaGa", "bilBao", "barCElona", "sEviLLa", "graNada"];
-// let i = 0;
+const myCities = ["maDriD", "MáLaGa", "bilBao", "barCElona", "sEviLLa", "graNada"];
+let i = 0;
 
-// console.log(myCities);
+console.log(myCities);
 
-// do {
-//     myCities[i] = myCities[i][0].toLocaleUpperCase() + myCities[i].substring(1).toLocaleLowerCase();;
-//     ++i;
-// }
-// while (i < myCities.length)
+do {
+    myCities[i] = myCities[i][0].toLocaleUpperCase() + myCities[i].substring(1).toLocaleLowerCase();
+    ++i;
+} while (i < myCities.length);
 
-// console.log(myCities);
+console.log(myCities);
 
 // 4. No Duplicates! A set is a collection of unique items. A set can be formed from an array by removing all duplicate items. Create a program which transforms an array into a set of unique values using a do while loop. See the examples below. 
 
@@ -58,27 +57,54 @@
 //     }
 
 //     ++i;
-// }
-// while (i < myNumbers.length)
+// } while (i < myNumbers.length);
 
 // console.log(uniqueItems);
 
 
 // 5. Repeat it. Create a program with two variables: "item" and "times". Create a program that repeats the "item" as many times as specified by "times". The first variable ("item") is the item that needs repeating while the second argument ("times") is the number of times the item is to be repeated. Print the result in an array.
 
-const repeatedItem = [];
-let i = 0;
+// const repeatedItem = [];
+// let i = 0;
 
-const program = (item, times) => {
+// const program = (item, times) => {
 
-    do {
-        repeatedItem.push(item);
-        ++i;
-    }
-    while (i < times);
+//     do {
+//         repeatedItem.push(item);
+//         ++i;
+//     }
+//     while (i < times);
 
 
-    return repeatedItem;
-}
+//     return repeatedItem;
+// }
 
-console.log(program("Hello", 5));
+// console.log(program("Hello", 5));
+
+
+// BONUSES - Choose the loop
+
+// Factors. A factor chain is an array where each previous element is a factor of the next consecutive element. The following is a factor chain: [3, 6, 12, 36]
+// Create a program that determines whether or not a given array is a factor chain using a do while loop.
+
+
+// const factorNumbers = [3, 7, 12, 36];
+// let i = 0;
+// let answer = "";
+
+// const myOperation = (itemOne, itemTwo) => {
+//     return itemTwo % itemOne === 0;
+// };
+
+// do {
+
+//     if (myOperation(factorNumbers[i], factorNumbers[i + 1]) === true) {
+//         answer = "TRUE";
+//     } else {
+//         answer = "FALSE";
+//     }
+
+//     ++i;
+// } while (i < factorNumbers.length);
+
+// console.log(answer);
---
+++ programming-basics-Jaime/loops/for-loops.js
@@ -0,0 +1,13 @@
+// FOR LOOPS 
+
+// 1. Addition: Write a program to add up the numbers 1 to 20.
+
+const addition = () => {
+    let total = 0;
+    for (let i = 0; i <= 20; ++i) {
+        total += i;
+    }
+    console.log(`Total: ${total}`);
+};
+console.log(`Exercise 1: Addition`)
+addition();
