5c6cc5861cf7cacf4d62ee3f0ab1d41ba689b299
Jaime Segura <jseguraweb@gmail.com>
2019-09-18 10:05:19 +0200

    add exercise 2 for loops: bottles of beer

--- programming-basics-Jaime/loops/for-loops.js
+++ programming-basics-Jaime/loops/for-loops.js
@@ -11,3 +11,37 @@ const addition = () => {
 };
 console.log(`Exercise 1: Addition`)
 addition();
+
+
+// 2. There are i bottles of beer on the wall. Write a program that will output, "There is one bottle of beer on the wall." "There are two bottles of beer on the wall" up until there are five bottles.
+
+const beerBottles = () => {
+    let total = 0;
+
+    for (let i = 1; i <= 5; ++i) {
+        total += 1;
+
+        if (total === 1) {
+            console.log(`There is one bottle of beer on the wall.`)
+        } else {
+            switch (i) {
+                case 2:
+                    total = "two";
+                    break;
+                case 3:
+                    total = "three";
+                    break;
+                case 4:
+                    total = "four";
+                    break;
+                case 5:
+                    total = "five";
+                    break;
+            }
+            console.log(`There are ${total} bottles of beer on the wall.`)
+        }
+    }
+
+};
+console.log(`\nExercise 2: beer bottles`)
+beerBottles();
