350631b505cc6480988d575775fa9ebd8af33ec5
Jaime Segura <jseguraweb@gmail.com>
2019-09-19 13:24:49 +0200

    add second and third loops of nested loops

--- programming-basics-Jaime/loops/nested-loops.js
+++ programming-basics-Jaime/loops/nested-loops.js
@@ -17,16 +17,32 @@ loopOne();
 
 // // EXERCISE 2.
 
-// const loopTwo = () => {
+const loopTwo = () => {
 
-//     let pipes = "";
-//     for (let i = 0; i < 4; ++i) {
-//         pipes = "";
-//         for (let j = 0; j < 5; ++j) {
-//             pipes += "|";
-//         }
-//         console.log(pipes);
-//     }
-// };
-// console.log(`\Second loop:`);
-// loopTwo();
+    let pipesAndDashes = "";
+    for (let i = 0; i < 3; ++i) {
+        pipesAndDashes = "";
+        if (i % 2 === 0) {
+            pipesAndDashes += "-|-|-";
+        } else {
+            pipesAndDashes += "|-|-|";
+        }
+        console.log(pipesAndDashes);
+    }
+};
+console.log(`\nSecond loop:`);
+loopTwo();
+
+
+// EXERCISE 3.
+
+const loopThree = () => {
+
+    let stars = "";
+    for (let i = 0; i < 4; ++i) {
+        stars += "*";
+        console.log(stars);
+    }
+};
+console.log(`\nThird loop:`);
+loopThree();
