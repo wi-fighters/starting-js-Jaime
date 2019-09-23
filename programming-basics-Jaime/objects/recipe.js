4a922a2563f763de5eb20a230c78cf901902434e
Jaime Segura <jseguraweb@gmail.com>
2019-09-23 11:23:48 +0200

    modify my recipe adding object and function inside an object

--- programming-basics-Jaime/objects/recipe.js
+++ programming-basics-Jaime/objects/recipe.js
@@ -1,7 +1,24 @@
 const myRecipe = new Object;
 myRecipe.name = "Spaghetti Carbonara";
-myRecipe.servings = 1;
-myRecipe.ingredients = ["125gr Spaghetti", "1 egg", "35gr Bacon", "40gr Parmesan"];
+myRecipe.onePerson = {
+    spaghetti: 125,
+    eggs: 1,
+    bacon: 40,
+    parmesan: 40
+};
+myRecipe.ingredients = (people) => {
+    if (people <= 1) {
+        return `${people * myRecipe.onePerson.spaghetti}gr spaghetti, ${people * myRecipe.onePerson.eggs} egg, ${people * myRecipe.onePerson.bacon}gr bacon, ${people * myRecipe.onePerson.parmesan}gr parmesan`;
+    } else {
+        return `${people * myRecipe.onePerson.spaghetti}gr spaghetti, ${people * myRecipe.onePerson.eggs} eggs, ${people * myRecipe.onePerson.bacon}gr bacon, ${people * myRecipe.onePerson.parmesan}gr parmesan`;
+    }
+};
 
-console.log(myRecipe);
+console.log(`What is the recipe?`);
 console.log(myRecipe.name);
+
+console.log(`\nWhat do I need to cook it for one person?`);
+console.log(myRecipe.ingredients(1));
+
+console.log(`\nWhat do I need to cook it for one person?`);
+console.log(myRecipe.ingredients(4));
