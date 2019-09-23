65967b54efeb1c81d81611ade6e66cbaa7914dce
Jaime Segura <jseguraweb@gmail.com>
2019-09-23 14:07:15 +0200

    modify exercise library with refactoring

--- programming-basics-Jaime/objects/library.js
+++ programming-basics-Jaime/objects/library.js
@@ -1,30 +1,72 @@
-const myBooks = [
-    {
-        title: "The Great Gatsby",
-        author: "F.Scott Fitzgerald",
-        alreadyRead: false
-    },
-    {
-        title: "To Kill a Mockingbird",
-        author: "Harper Lee",
-        alreadyRead: false
-    },
-    {
-        title: "Harry Potter and the Sorcerer's Stone",
-        author: "J.K.Rowling",
-        alreadyRead: true
-    },
-    {
-        title: "1984",
-        author: "George Orwell",
-        alreadyRead: true
-    },
-];
+const libraryOne = () => {
+    const myBooks = [
+        {
+            title: "The Great Gatsby",
+            author: "F.Scott Fitzgerald",
+            alreadyRead: false
+        },
+        {
+            title: "To Kill a Mockingbird",
+            author: "Harper Lee",
+            alreadyRead: false
+        },
+        {
+            title: "Harry Potter and the Sorcerer's Stone",
+            author: "J.K.Rowling",
+            alreadyRead: true
+        },
+        {
+            title: "1984",
+            author: "George Orwell",
+            alreadyRead: true
+        }
+    ];
 
-for (let i = 0; i < myBooks.length; ++i) {
-    if (myBooks[i].alreadyRead === true) {
-        console.log(`You've already read ${myBooks[i].title} by ${myBooks[i].author}`)
-    } else {
-        console.log(`You still need to read ${myBooks[i].title} by ${myBooks[i].author}`)
+    for (let i = 0; i < myBooks.length; ++i) {
+        if (myBooks[i].alreadyRead === true) {
+            console.log(`You've already read ${myBooks[i].title} by ${myBooks[i].author}`)
+        } else {
+            console.log(`You still need to read ${myBooks[i].title} by ${myBooks[i].author}`)
+        }
     }
-}
+};
+console.log(`\nFirst example:`);
+libraryOne();
+
+// --------------------------------------------------
+
+const libraryTwo = () => {
+    const books = [];
+    books.push(
+        {
+            title: "The Great Gatsby",
+            author: "F.Scott Fitzgerald",
+            alreadyRead: false
+        },
+        {
+            title: "To Kill a Mockingbird",
+            author: "Harper Lee",
+            alreadyRead: false
+        },
+        {
+            title: "Harry Potter and the Sorcerer's Stone",
+            author: "J.K.Rowling",
+            alreadyRead: true
+        },
+        {
+            title: "1984",
+            author: "George Orwell",
+            alreadyRead: true
+        }
+    );
+
+    for (let i = 0; i < books.length; ++i) {
+        if (books[i].alreadyRead === true) {
+            console.log(`You've already read ${books[i].title} by ${books[i].author}`)
+        } else {
+            console.log(`You still need to read ${books[i].title} by ${books[i].author}`)
+        }
+    }
+};
+console.log(`\nRefactored:`);
+libraryTwo();
