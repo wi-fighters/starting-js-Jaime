a17115434fe8ff23f38ee096b03046f2133adcd0
Jaime Segura <jseguraweb@gmail.com>
2019-09-23 16:00:16 +0200

    add properties to my penguin

--- programming-basics-Jaime/objects/the-penguin.js
+++ programming-basics-Jaime/objects/the-penguin.js
@@ -1,1 +1,6 @@
 const myPenguin = new Object();
+myPenguin["character's name"] = "Misha";
+myPenguin.origin = "Death and the Penguin, Penguin Lost";
+myPenguin.author = "Andrey Kurkov";
+
+console.log(myPenguin);
