67aa75706ce56c5f3749c965cf747eecd2e4cbb4
Jaime Segura <jseguraweb@gmail.com>
2019-09-23 13:16:43 +0200

    create array with objects

---
+++ programming-basics-Jaime/objects/library.js
@@ -0,0 +1,22 @@
+const myBooks = [
+    {
+        title: "The Great Gatsby",
+        author: "F.Scott Fitzgerald",
+        alreadyRead: false
+    },
+    {
+        title: "To Kill a Mockingbird",
+        author: "Harper Lee",
+        alreadyRead: false
+    },
+    {
+        title: "Harry Potter and the Sorcerer's Stone",
+        author: "J.K.Rowling",
+        alreadyRead: false
+    },
+    {
+        title: "1984",
+        author: "George Orwell",
+        alreadyRead: false
+    },
+];
