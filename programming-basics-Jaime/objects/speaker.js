f610e01a94be189a16b54dc833e7a0c7ea3c37e5
Jaime Segura <jseguraweb@gmail.com>
2019-09-24 13:45:33 +0200

    create function addSpeaker to add a speaker to the array of speakers

--- programming-basics-Jaime/objects/speaker.js
+++ programming-basics-Jaime/objects/speaker.js
@@ -19,5 +19,15 @@ const nestedObject = {
                 hello: "Bonjour"
             }
         }
+    },
+    addSpeaker: function (speaker) {
+        this.speakers.push(speaker);
     }
 };
+
+// 1-Write a function addSpeaker to add a speaker to the array of speakers. The speaker you add must be an object with a key of name and a value of whatever you’d like.
+
+console.log(nestedObject.speakers);
+const jaime = { name: "Jaime" };
+nestedObject.addSpeaker(jaime);
+console.log(nestedObject.speakers);
