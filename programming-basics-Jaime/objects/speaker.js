616968cab4c59156196bfa7121b7d4bedaffdcc7
Jaime Segura <jseguraweb@gmail.com>
2019-09-24 15:06:57 +0200

    create function addCountry that adds a European country to the countries object

--- programming-basics-Jaime/objects/speaker.js
+++ programming-basics-Jaime/objects/speaker.js
@@ -20,11 +20,14 @@ const nestedObject = {
             }
         }
     },
-    addSpeaker: function (speaker) {
-        this.speakers.push(speaker);
+    addSpeaker: function (newSpeaker) {
+        this.speakers.push(newSpeaker);
     },
     addLanguage: function (newLang, language) {
         this.data.languages[newLang] = language;
+    },
+    addCountry: function (newCountry, country) {
+        this.data.continents.europe.countries[newCountry] = country;
     }
 };
 
@@ -35,3 +38,9 @@ console.log(nestedObject.speakers);
 const german = { hello: "Hallo" };
 nestedObject.addLanguage("german", german);
 console.log(nestedObject.data.languages);
+
+// 3-Write a function addCountry that adds a European country to the countries object (inside of the continents object, inside of the countries object). The country you add should be an object with the key as name of the country and the value as an object with the keys of “capital” and “population” and their respective values.
+
+const spain = { capital: "Madrid", population: 45000000 };
+nestedObject.addCountry("spain", spain);
+console.log(nestedObject.data.continents.europe.countries);
