59bffb00d80c02e288305c0b705a5ee367d02293
Jaime Segura <jseguraweb@gmail.com>
2019-09-24 15:08:01 +0200

    refactor object

--- programming-basics-Jaime/objects/speaker.js
+++ programming-basics-Jaime/objects/speaker.js
@@ -39,8 +39,6 @@ const german = { hello: "Hallo" };
 nestedObject.addLanguage("german", german);
 console.log(nestedObject.data.languages);
 
-// 3-Write a function addCountry that adds a European country to the countries object (inside of the continents object, inside of the countries object). The country you add should be an object with the key as name of the country and the value as an object with the keys of “capital” and “population” and their respective values.
-
 const spain = { capital: "Madrid", population: 45000000 };
 nestedObject.addCountry("spain", spain);
 console.log(nestedObject.data.continents.europe.countries);
