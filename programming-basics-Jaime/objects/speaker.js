16ce6e157343ca9b00e07971becc38fb96725dea
Jaime Segura <jseguraweb@gmail.com>
2019-09-24 13:38:37 +0200

    start speaker exercise

---
+++ programming-basics-Jaime/objects/speaker.js
@@ -0,0 +1,23 @@
+const nestedObject = {
+    speakers: [{ name: "Elie" }, { name: "Tim" }, { name: "Matt" }],
+    data: {
+        continents: {
+            europe: {
+                countries: {
+                    switzerland: {
+                        capital: "Bern",
+                        population: 8000000
+                    }
+                }
+            }
+        },
+        languages: {
+            spanish: {
+                hello: "Hola"
+            },
+            french: {
+                hello: "Bonjour"
+            }
+        }
+    }
+};
