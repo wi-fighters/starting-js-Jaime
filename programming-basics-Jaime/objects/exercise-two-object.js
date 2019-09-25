cf07f32c18aaf910517bd04f62ebb6cdb7a3f98f
Jaime Segura <jseguraweb@gmail.com>
2019-09-25 20:09:56 +0200

    add 5 objects

---
+++ programming-basics-Jaime/objects/exercise-two-object.js
@@ -0,0 +1,47 @@
+
+// const person;
+// console.log(person.firstName);
+// console.log(person.lastName);
+// console.log(person.age);
+// console.log(person.employed);
+
+// 1 - change what needed to ADD more people to your array of objects, you need at least 5 people
+// 2 - Find how many people has a job
+// 3 - display only the people who are over 30 and have no job
+
+const people = [person_1, person_2, person_3, person_4, person_5];
+
+const person_1 = {
+    firstName: "Peter",
+    lastName: "Poop",
+    age: 25,
+    employed: true,
+};
+
+const person_2 = {
+    firstName: "Michael",
+    lastName: "Night",
+    age: 35,
+    employed: false,
+};
+
+const person_3 = {
+    firstName: "Lara",
+    lastName: "Croft",
+    age: 31,
+    employed: true,
+};
+
+const person_4 = {
+    firstName: "Steven",
+    lastName: "Seagal",
+    age: 45,
+    employed: false,
+};
+
+const person_5 = {
+    firstName: "Sarah",
+    lastName: "Connor",
+    age: 25,
+    employed: true,
+};
