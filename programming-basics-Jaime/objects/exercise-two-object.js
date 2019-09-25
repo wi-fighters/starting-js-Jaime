e09954699e9ad7f112ff36545f5869c9950ea231
Jaime Segura <jseguraweb@gmail.com>
2019-09-25 20:34:56 +0200

    add a function to find how many people have a job

--- programming-basics-Jaime/objects/exercise-two-object.js
+++ programming-basics-Jaime/objects/exercise-two-object.js
@@ -6,42 +6,59 @@
 // console.log(person.employed);
 
 // 1 - change what needed to ADD more people to your array of objects, you need at least 5 people
-// 2 - Find how many people has a job
+// 2 - Find how many people have a job
 // 3 - display only the people who are over 30 and have no job
 
-const people = [person_1, person_2, person_3, person_4, person_5];
-
-const person_1 = {
+const person1 = {
     firstName: "Peter",
-    lastName: "Poop",
+    lastName: "Griffin",
     age: 25,
     employed: true,
 };
 
-const person_2 = {
+const person2 = {
     firstName: "Michael",
     lastName: "Night",
     age: 35,
     employed: false,
 };
 
-const person_3 = {
+const person3 = {
     firstName: "Lara",
     lastName: "Croft",
     age: 31,
     employed: true,
 };
 
-const person_4 = {
+const person4 = {
     firstName: "Steven",
     lastName: "Seagal",
     age: 45,
     employed: false,
 };
 
-const person_5 = {
+const person5 = {
     firstName: "Sarah",
     lastName: "Connor",
     age: 25,
     employed: true,
 };
+
+const people = [person1, person2, person3, person4, person5];
+
+// How many people have a job?
+
+const findUnemployed = () => {
+    let unemployedPeople = "";
+    for (let i = 0; i < people.length; ++i) {
+        unemployedPeople = "";
+        if (people[i].employed === true) {
+            console.log(unemployedPeople += `- ${people[i].firstName} ${people[i].lastName}`);
+        }
+    }
+};
+
+console.log(`\nThese are the people of the group who have a job:`);
+findUnemployed();
+
+// Who is over 30 and has no job?
