878f0fd08fc99f1f928e7656bbfeb33e0c573044
Jaime Segura <jseguraweb@gmail.com>
2019-10-21 21:54:21 +0200

    instantiate the constructor by creating 3 new books

--- programming-basics-Jaime/objects/book-shelf.js
+++ programming-basics-Jaime/objects/book-shelf.js
@@ -12,3 +12,7 @@ class Book {
         return `Author: ${this.author}`;
     }
 }
+
+const PP = new Book("Pride and Prejudice", "Jane Austen");
+const H = new Book("Hamlet", "William Shakespeare");
+const WP = new Book("War and Peace", "Leo Tolstoy");
