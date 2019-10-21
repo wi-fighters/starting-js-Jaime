11d31b57739eadd9f07b5efab015ab129f6dd3e4
Jaime Segura <jseguraweb@gmail.com>
2019-10-21 22:50:58 +0200

    solve test-jackpot

--- programming-basics-Jaime/objects/book-shelf.js
+++ programming-basics-Jaime/objects/book-shelf.js
@@ -16,3 +16,18 @@ class Book {
 const PP = new Book("Pride and Prejudice", "Jane Austen");
 const H = new Book("Hamlet", "William Shakespeare");
 const WP = new Book("War and Peace", "Leo Tolstoy");
+
+console.log(PP.title);
+console.log(PP.author);
+console.log(PP.getTitle());
+console.log(PP.getAuthor());
+
+console.log(H.title);
+console.log(H.author);
+console.log(H.getTitle());
+console.log(H.getAuthor());
+
+console.log(WP.title);
+console.log(WP.author);
+console.log(WP.getTitle());
+console.log(WP.getAuthor());
